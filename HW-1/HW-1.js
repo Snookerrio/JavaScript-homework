b46a5a1990448f706ebbc8595f77253f006d24da
snookerrio <yurakolodka086@gmail.com>
2024-04-29 23:34:00 +0300

    HW-1

---
+++ HW-1/HW-1.js
@@ -0,0 +1,25 @@
+let srt1 ='hello';
+let srt2 = 'owu';
+let srt3= 'com';
+let srt4='ua';
+let num1=1;
+let num2= 10;
+let num3= -999;
+let num4= 123;
+let num5= 3.14;
+let num6= 2.7;
+let num7= 16;
+let bool1=true;
+let bool2=false;
+console.log(srt1,srt2,srt3,srt4,num1,num2,num3,num4,num5,num6,num7,bool1,bool2);
+
+let firstName='Kolodka';
+let middleName='Yuri';
+let lastName='Ruslanovich';
+let person=firstName+' '+ middleName+' '+lastName
+console.log(person)
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a,typeof b,typeof c);
